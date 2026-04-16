b080944af3ca7073fc9e580b4bee6d96fb6eceec
llorichT <anfimovallori@yandex.ru>
2026-04-16 13:34:07 +0300

    исправление замечаний

--- tests.py
+++ tests.py
@@ -1,27 +1,3 @@
-from main import BooksCollector
-
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
-class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 import pytest
 from main import BooksCollector
 
@@ -107,3 +83,17 @@ class TestBooksCollector:
         collector.delete_book_from_favorites('1984')
 
         assert '1984' not in collector.get_list_of_favorites_books()
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+
+        assert collector.get_books_genre() == {'1984': ''}
+
+
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+        collector.set_book_genre('1984', 'Фантастика')
+
+        assert collector.get_book_genre('1984') == 'Фантастика'
